14eb74feee4c5b865e817c5f2601b71c333bdd7e
alyson <consulialyson@gmail.com>
2022-07-27 17:15:18 -0300

    feat: cypress create recommendation

---
+++ front-end/cypress/e2e/recommendationsTests/app.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+import { faker } from "@faker-js/faker"
+
+const URL = "http://localhost:3000"
+
+describe("app test", () => {
+  it("should create a recommendation", () => {
+    const recommendation = {
+      name: faker.music.songName(),
+      youtubeLink: "https://www.youtube.com/watch?v=E1jRjGhohpA",
+    }
+    cy.visit(`${URL}/`)
+    cy.get("#name").type(recommendation.name)
+    cy.get("#youtubeLink").type(recommendation.youtubeLink)
+
+    cy.intercept("POST", "/").as("postRecommendation")
+    cy.get("#createRecommendation").click()
+    cy.wait("@postRecommendation")
+
+    cy.url().should("equal", `${URL}/`)
+  })
+})
