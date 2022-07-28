a66fcb2d92806a4e3a3ae25ac751783b9b1aeb81
alyson <consulialyson@gmail.com>
2022-07-28 14:21:37 -0300

    feat: command to create recommendation

--- front-end/cypress/e2e/recommendationsTests/app.cy.js
+++ front-end/cypress/e2e/recommendationsTests/app.cy.js
@@ -5,15 +5,16 @@ import { faker } from "@faker-js/faker"
 const URL = "http://localhost:3000"
 
 describe("app test", () => {
+  const recommendation = {
+    name: faker.music.songName(),
+    youtubeLink: "https://www.youtube.com/watch?v=E1jRjGhohpA",
+  }
+
   beforeEach(() => {
     cy.resetRecommendations()
   })
 
   it("should create a recommendation", () => {
-    const recommendation = {
-      name: faker.music.songName(),
-      youtubeLink: "https://www.youtube.com/watch?v=E1jRjGhohpA",
-    }
     cy.visit(`${URL}/`)
     cy.get("#name").type(recommendation.name)
     cy.get("#youtubeLink").type(recommendation.youtubeLink)
--- front-end/cypress/support/commands.js
+++ front-end/cypress/support/commands.js
@@ -2,3 +2,13 @@ Cypress.Commands.add("resetRecommendations", () => {
   cy.log("reset recommendations")
   cy.request("POST", "http://localhost:5000/recommendations/reset")
 })
+
+Cypress.Commands.add("createRecommendation", (recommendation) => {
+  cy.request(
+    "POST",
+    "http://localhost:5000/recommendations",
+    recommendation,
+  ).then((res) => {
+    cy.log(res)
+  })
+})
