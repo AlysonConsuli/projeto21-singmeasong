6d121dbdf74a6bad17ba0d4c92dbf8297de311e8
alyson <consulialyson@gmail.com>
2022-07-29 14:59:27 -0300

    feat: unit test duplicate recommendation

--- back-end/tests/unit/recommendationService.test.ts
+++ back-end/tests/unit/recommendationService.test.ts
@@ -17,4 +17,15 @@ describe("recommendationService test suite", () => {
     await recommendationService.insert(recommendation)
     expect(recommendationRepository.create).toBeCalled()
   })
+
+  it("given a recommendation name equals to another, return conflict error", async () => {
+    jest
+      .spyOn(recommendationRepository, "findByName")
+      .mockResolvedValueOnce(recommendation as any)
+    const promise = recommendationService.insert(recommendation)
+    expect(promise).rejects.toEqual({
+      type: "conflict",
+      message: "Recommendations names must be unique",
+    })
+  })
 })
