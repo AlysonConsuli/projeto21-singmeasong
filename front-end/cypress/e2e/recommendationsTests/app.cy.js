57b5922f36380d5e40d4106d22c91aa7b5a46568
alyson <consulialyson@gmail.com>
2022-07-30 15:59:14 -0300

    feat: cypress handle post recommendation errors

--- front-end/cypress/e2e/recommendationsTests/app.cy.js
+++ front-end/cypress/e2e/recommendationsTests/app.cy.js
@@ -3,17 +3,16 @@
 import { faker } from "@faker-js/faker"
 
 const URL = "http://localhost:3000"
+const recommendation = {
+  name: faker.music.songName(),
+  youtubeLink: "https://www.youtube.com/watch?v=E1jRjGhohpA",
+}
 
-describe("app test", () => {
-  const recommendation = {
-    name: faker.music.songName(),
-    youtubeLink: "https://www.youtube.com/watch?v=E1jRjGhohpA",
-  }
+beforeEach(() => {
+  cy.resetRecommendations()
+})
 
-  beforeEach(() => {
-    cy.resetRecommendations()
-  })
-
+describe("post recommendations tests", () => {
   it("should create a recommendation", () => {
     cy.visit(`${URL}/`)
     cy.get("#name").type(recommendation.name)
@@ -24,6 +23,29 @@ describe("app test", () => {
     cy.wait("@postRecommendation").its("response.statusCode").should("eq", 201)
   })
 
+  it("given a recommendation with same name that other, receive 409", () => {
+    cy.createRecommendation(recommendation)
+    cy.visit(`${URL}/`)
+    cy.get("#name").type(recommendation.name)
+    cy.get("#youtubeLink").type(recommendation.youtubeLink)
+
+    cy.intercept("POST", "/recommendations").as("postRecommendation")
+    cy.get("#createRecommendation").click()
+    cy.wait("@postRecommendation").its("response.statusCode").should("eq", 409)
+  })
+
+  it("given a link that is not from youtube, receive 422", () => {
+    cy.visit(`${URL}/`)
+    cy.get("#name").type(recommendation.name)
+    cy.get("#youtubeLink").type("https://www.google.com/")
+
+    cy.intercept("POST", "/recommendations").as("postRecommendation")
+    cy.get("#createRecommendation").click()
+    cy.wait("@postRecommendation").its("response.statusCode").should("eq", 422)
+  })
+})
+
+describe("post recommendations upvote/downvote tests", () => {
   it("should upvote by 1", () => {
     cy.createRecommendation(recommendation)
     cy.getRecommendation().then((res) => {
