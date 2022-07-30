b728920eb3437d0ab8abbe69effad6ef40c27a47
alyson <consulialyson@gmail.com>
2022-07-30 18:16:58 -0300

    feat: cypress get top recommendations

--- front-end/cypress/e2e/recommendationsTests/app.cy.js
+++ front-end/cypress/e2e/recommendationsTests/app.cy.js
@@ -12,91 +12,91 @@ beforeEach(() => {
   cy.resetRecommendations()
 })
 
-// describe("post recommendations tests", () => {
-//   it("should create a recommendation", () => {
-//     cy.visit(`${URL}/`)
-//     cy.get("#name").type(recommendation.name)
-//     cy.get("#youtubeLink").type(recommendation.youtubeLink)
+describe("post recommendations tests", () => {
+  it("should create a recommendation", () => {
+    cy.visit(`${URL}/`)
+    cy.get("#name").type(recommendation.name)
+    cy.get("#youtubeLink").type(recommendation.youtubeLink)
 
-//     cy.intercept("POST", "/recommendations").as("postRecommendation")
-//     cy.get("#createRecommendation").click()
-//     cy.wait("@postRecommendation").its("response.statusCode").should("eq", 201)
-//   })
+    cy.intercept("POST", "/recommendations").as("postRecommendation")
+    cy.get("#createRecommendation").click()
+    cy.wait("@postRecommendation").its("response.statusCode").should("eq", 201)
+  })
 
-//   it("given a recommendation with same name that other, receive 409", () => {
-//     cy.createRecommendation(recommendation)
-//     cy.visit(`${URL}/`)
-//     cy.get("#name").type(recommendation.name)
-//     cy.get("#youtubeLink").type(recommendation.youtubeLink)
+  it("given a recommendation with same name that other, receive 409", () => {
+    cy.createRecommendation(recommendation)
+    cy.visit(`${URL}/`)
+    cy.get("#name").type(recommendation.name)
+    cy.get("#youtubeLink").type(recommendation.youtubeLink)
 
-//     cy.intercept("POST", "/recommendations").as("postRecommendation")
-//     cy.get("#createRecommendation").click()
-//     cy.wait("@postRecommendation").its("response.statusCode").should("eq", 409)
-//   })
+    cy.intercept("POST", "/recommendations").as("postRecommendation")
+    cy.get("#createRecommendation").click()
+    cy.wait("@postRecommendation").its("response.statusCode").should("eq", 409)
+  })
 
-//   it("given a link that is not from youtube, receive 422", () => {
-//     cy.visit(`${URL}/`)
-//     cy.get("#name").type(recommendation.name)
-//     cy.get("#youtubeLink").type("https://www.google.com/")
+  it("given a link that is not from youtube, receive 422", () => {
+    cy.visit(`${URL}/`)
+    cy.get("#name").type(recommendation.name)
+    cy.get("#youtubeLink").type("https://www.google.com/")
 
-//     cy.intercept("POST", "/recommendations").as("postRecommendation")
-//     cy.get("#createRecommendation").click()
-//     cy.wait("@postRecommendation").its("response.statusCode").should("eq", 422)
-//   })
-// })
+    cy.intercept("POST", "/recommendations").as("postRecommendation")
+    cy.get("#createRecommendation").click()
+    cy.wait("@postRecommendation").its("response.statusCode").should("eq", 422)
+  })
+})
 
-// describe("post recommendations upvote/downvote tests", () => {
-//   it("should upvote by 1", () => {
-//     cy.createRecommendation(recommendation)
-//     cy.getRecommendation().then((res) => {
-//       cy.visit(`${URL}/`)
+describe("post recommendations upvote/downvote tests", () => {
+  it("should upvote by 1", () => {
+    cy.createRecommendation(recommendation)
+    cy.getRecommendation().then((res) => {
+      cy.visit(`${URL}/`)
 
-//       cy.intercept("POST", `/recommendations/${res.id}/upvote`).as("upvote")
-//       cy.get("article").find("div:last-child").find("svg:first-child").click()
-//       cy.wait("@upvote").its("response.statusCode").should("eq", 200)
+      cy.intercept("POST", `/recommendations/${res.id}/upvote`).as("upvote")
+      cy.get("article").find("div:last-child").find("svg:first-child").click()
+      cy.wait("@upvote").its("response.statusCode").should("eq", 200)
 
-//       cy.getRecommendation().then((res2) => {
-//         expect(res2.score).equal(res.score + 1)
-//       })
-//     })
-//   })
+      cy.getRecommendation().then((res2) => {
+        expect(res2.score).equal(res.score + 1)
+      })
+    })
+  })
 
-//   it("should downvote by 1", () => {
-//     cy.createRecommendation(recommendation)
-//     cy.getRecommendation().then((res) => {
-//       cy.visit(`${URL}/`)
+  it("should downvote by 1", () => {
+    cy.createRecommendation(recommendation)
+    cy.getRecommendation().then((res) => {
+      cy.visit(`${URL}/`)
 
-//       cy.intercept("POST", `/recommendations/${res.id}/downvote`).as("downvote")
-//       cy.get("article").find("div:last-child").find("svg:last-child").click()
-//       cy.wait("@downvote").its("response.statusCode").should("eq", 200)
+      cy.intercept("POST", `/recommendations/${res.id}/downvote`).as("downvote")
+      cy.get("article").find("div:last-child").find("svg:last-child").click()
+      cy.wait("@downvote").its("response.statusCode").should("eq", 200)
 
-//       cy.getRecommendation().then((res2) => {
-//         expect(res2.score).equal(res.score - 1)
-//       })
-//     })
-//   })
+      cy.getRecommendation().then((res2) => {
+        expect(res2.score).equal(res.score - 1)
+      })
+    })
+  })
 
-//   it("should remove recommendation", () => {
-//     cy.createRecommendation(recommendation)
-//     cy.getRecommendation().then((res) => {
-//       cy.visit(`${URL}/`)
-//       cy.intercept("POST", `/recommendations/${res.id}/downvote`).as("downvote")
-//       for (let i = 0; i < 5; i++) {
-//         cy.get("article").find("div:last-child").find("svg:last-child").click()
-//       }
-//       cy.wait("@downvote").its("response.statusCode").should("eq", 200)
+  it("should remove recommendation", () => {
+    cy.createRecommendation(recommendation)
+    cy.getRecommendation().then((res) => {
+      cy.visit(`${URL}/`)
+      cy.intercept("POST", `/recommendations/${res.id}/downvote`).as("downvote")
+      for (let i = 0; i < 5; i++) {
+        cy.get("article").find("div:last-child").find("svg:last-child").click()
+      }
+      cy.wait("@downvote").its("response.statusCode").should("eq", 200)
 
-//       cy.intercept("POST", `/recommendations/${res.id}/downvote`).as("remove")
-//       cy.get("article").find("div:last-child").find("svg:last-child").click()
-//       cy.wait("@remove").its("response.statusCode").should("eq", 200)
+      cy.intercept("POST", `/recommendations/${res.id}/downvote`).as("remove")
+      cy.get("article").find("div:last-child").find("svg:last-child").click()
+      cy.wait("@remove").its("response.statusCode").should("eq", 200)
 
-//       cy.get("div").should(
-//         "contain",
-//         "No recommendations yet! Create your own :)",
-//       )
-//     })
-//   })
-// })
+      cy.get("div").should(
+        "contain",
+        "No recommendations yet! Create your own :)",
+      )
+    })
+  })
+})
 
 describe("get recommendations tests", () => {
   it("should return the last 10 recommendations", () => {
@@ -122,5 +122,22 @@ describe("get recommendations tests", () => {
     cy.get("article")
       .find("div:first-child")
       .should("contain", recommendation.name)
+  })
+
+  it("should get recommendations order by score", () => {
+    const recommendationsQty = +faker.random.numeric(1, {
+      bannedDigits: ["0", "1"],
+    })
+    cy.createManyRecommendations(recommendationsQty, recommendation)
+
+    cy.visit(`${URL}/top`)
+    cy.intercept("POST", `/recommendations/**`).as("upvote")
+    cy.get("article:last-child")
+      .find("div:last-child")
+      .find("svg:first-child")
+      .click()
+    cy.wait("@upvote").its("response.statusCode").should("eq", 200)
+
+    cy.get("article:nth-child(3)").find("div:last-child").should("contain", "1")
   })
 })
--- front-end/cypress/support/commands.js
+++ front-end/cypress/support/commands.js
@@ -1,29 +1,25 @@
+const BACKEND_URL = "http://localhost:5000"
+
 Cypress.Commands.add("resetRecommendations", () => {
   cy.log("reset recommendations")
-  cy.request("POST", "http://localhost:5000/recommendations/reset")
+  cy.request("POST", `${BACKEND_URL}/recommendations/reset`)
 })
 
 Cypress.Commands.add("createRecommendation", (recommendation) => {
-  cy.request(
-    "POST",
-    "http://localhost:5000/recommendations",
-    recommendation,
-  ).then((res) => {
-    cy.log(res)
-  })
-})
-
-Cypress.Commands.add("getRecommendation", () => {
-  cy.request("GET", "http://localhost:5000/recommendations").then((res) => {
-    return res.body[0]
-  })
+  cy.request("POST", `${BACKEND_URL}/recommendations`, recommendation)
 })
 
 Cypress.Commands.add("createManyRecommendations", (qty, recommendation) => {
   for (let i = 0; i < qty; i++) {
-    cy.request("POST", "http://localhost:5000/recommendations", {
+    cy.request("POST", `${BACKEND_URL}/recommendations`, {
       ...recommendation,
       name: `${recommendation.name} ${i}`,
     })
   }
+})
+
+Cypress.Commands.add("getRecommendation", () => {
+  cy.request("GET", `${BACKEND_URL}/recommendations`).then((res) => {
+    return res.body[0]
+  })
 })
